5da11b52c52c6f6d536da62e5890e3836b45f109
Susufung <suzzax@gmail.com>
2020-05-14 09:18:37 +1000

    cancancan functioning well

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 class ListingsController < ApplicationController 
   
   before_action :find_listing, only:[:show, :edit, :update, :destroy]
-  # before_action :authenticate_user! only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   load_and_authorize_resource
   
   def index
