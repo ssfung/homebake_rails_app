a0cd1d540dfea5beef1f427c9e7ac8ebf6e96588
Susufung <suzzax@gmail.com>
2020-05-14 22:37:09 +1000

    added stripe

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem "table_print"
 gem "aws-sdk-s3", "~> 1.64"
 
 gem "cancancan", "~> 3.1"
+
+gem "stripe", "~> 5.22"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_145027) do
+ActiveRecord::Schema.define(version: 2020_05_14_012135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_05_12_145027) do
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "listings", force: :cascade do |t|
