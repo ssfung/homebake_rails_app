a87570e3ff20cc214befdc9b2450baaa08061416
Susufung <suzzax@gmail.com>
2020-05-16 17:11:44 +1000

    changes to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,10 @@
 class CartsController < ApplicationController
   def create
-    cart = if !current_user.cart
-             Cart.create(user_id: current_user.id)
-           else
-             current_user.cart
-           end
-    listing = Listing.find.build(params[:listing_id])
-    cart.listing_carts.create(listing: listing)
-    redirect_to listings_path
+  cart = Cart.new(completed: false)
+  if cart.save
+  listing = Listing.find(params[:listing_id])
+  cart.listings << listing 
+  redirect_to listings_path
   end
+end 
 end 
