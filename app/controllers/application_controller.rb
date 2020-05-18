3cdbc04b427b4cb6172ed15426a2311de0f27fc1
Susufung <suzzax@gmail.com>
2020-05-18 15:25:17 +1000

    updated the styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-before_action :configure_custom_parameters, if: :devise_controller?
+  before_action :configure_custom_parameters, if: :devise_controller?
 
- rescue_from CanCan::AccessDenied do |exception|
-     flash[:authorization_error] = "Not authorized to perform that action"
-     redirect_to listings_path
-   end 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:authorization_error] = "Not authorized to perform that action"
+    redirect_to listings_path
+  end
 
-protected
+  protected
 
   def configure_custom_parameters
-  devise_parameter_sanitizer.permit(:sign_up) do |params|
-    params.permit(:name, :email, :password, :password_confirmation)
-  end 
+    devise_parameter_sanitizer.permit(:sign_up) do |params|
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
   end
-end 
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,21 +3,19 @@ class Listing < ApplicationRecord
   # validates :description, presence: true
   # validates :price, presence: true
   # validates :quantity, presence: true
-  
-  
-  
+
   has_many :listings_categories
   has_many :categories, through: :listings_categories
   has_one_attached :picture
-  has_many :carts, through: :carts_listings 
-  belongs_to :user 
+  has_many :carts, through: :carts_listings
+  belongs_to :user
   has_many :carts_listings
-end 
-  
-  # validates :title, presence: true
-  # validates :description, presence: true
-  # validates :price, presence: true
-  # validates :quantity, presence: true
-  # validates :delivery, presence: true
-  # validates :gluten_free, presence: true
-  # validates :dairy_free, presence: true
+end
+
+# validates :title, presence: true
+# validates :description, presence: true
+# validates :price, presence: true
+# validates :quantity, presence: true
+# validates :delivery, presence: true
+# validates :gluten_free, presence: true
+# validates :dairy_free, presence: true
