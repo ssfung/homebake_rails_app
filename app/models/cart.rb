dbecea900ad9f662583b94a2d71255e1d70ac8d0
Susufung <suzzax@gmail.com>
2020-05-20 19:06:55 +1000

    debugging heroku errors

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :carts_listings, dependent: :destroy
-  has_many :listings, through: :carts_listings, 
+  has_many :listings, through: :carts_listings
 end
