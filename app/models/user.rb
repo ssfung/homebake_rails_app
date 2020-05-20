da6272e51f0b1ce04a54b743c62e9f4f0e8d7fa0
Susufung <suzzax@gmail.com>
2020-05-20 19:02:04 +1000

    changed dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :listings
+  has_many :listings, dependent: :destroy
   has_many :carts
   validates :name, presence: true
 end
