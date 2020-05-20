e77d8234056c4f9fa08bc7d4edf766711837baba
Susufung <suzzax@gmail.com>
2020-05-20 18:24:11 +1000

    changed dependent relationship

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,7 @@ class Listing < ApplicationRecord
   has_many :listings_categories, dependent: :destroy
   has_many :categories, through: :listings_categories
   has_one_attached :picture
-  has_many :carts, through: :carts_listings
+  has_many :carts, through: :carts_listings, dependent: :destroy
   has_many :carts_listings, dependent: :destroy
   belongs_to :user
 end
