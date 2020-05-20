a8a48a58e02009892a6d2084b18a6c71ca75d1e8
Susufung <suzzax@gmail.com>
2020-05-20 18:58:07 +1000

    fixed up depenndenacies

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
   has_many :listings_categories, dependent: :destroy
-  has_many :listings, through: :listings_categories, dependent: :destroy
-
+  has_many :listings, through: :listings_categories
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,9 +5,9 @@ class Listing < ApplicationRecord
   validates :picture, presence:true
 
   has_many :listings_categories, dependent: :destroy
-  has_many :categories, through: :listings_categories, dependent: :destroy
+  has_many :categories, through: :listings_categories
   has_one_attached :picture
-  has_many :carts, through: :carts_listings, dependent: :destroy
+  has_many :carts, through: :carts_listings
   has_many :carts_listings, dependent: :destroy
   belongs_to :user
 end
