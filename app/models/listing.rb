7dfc94e6418682b1232bd21f37c5e96f614386b9
Susufung <suzzax@gmail.com>
2020-05-20 18:51:57 +1000

    changing schema

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ApplicationRecord
   validates :picture, presence:true
 
   has_many :listings_categories, dependent: :destroy
-  has_many :categories, through: :listings_categories
+  has_many :categories, through: :listings_categories, dependent: :destroy
   has_one_attached :picture
   has_many :carts, through: :carts_listings, dependent: :destroy
   has_many :carts_listings, dependent: :destroy
