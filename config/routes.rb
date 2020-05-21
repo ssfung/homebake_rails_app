d3f307e92ab437e930fb68ce8626ad4a089c0926
Susufung <suzzax@gmail.com>
2020-05-21 12:29:48 +1000

    added code comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "/payments/session", to: "payments#get_stripe_id"
   get "/payments/success", to: "payments#success"
   post "/payments/webhook", to: "payments#webhook"
+ 
 end
  
