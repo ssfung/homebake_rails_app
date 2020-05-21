16ada83d03f02d4735b10dbcd4c90df9fba07778
Susufung <suzzax@gmail.com>
2020-05-21 11:44:07 +1000

    added santisation

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,8 +2,10 @@ class ListingsController < ApplicationController
   before_action :find_listing, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   load_and_authorize_resource
+  include ActionView::Helpers::SanitizeHelper
 
   def index
+    # the @q instance variable inherits from the application controller method set_search. This will allow all the listings to be searched. When it is being searched, eager loading is being activated as it will search for listings with images and categories.
     @q = Listing.ransack(params[:q])
     @listings = @q.result(distinct: true).includes(:categories).with_attached_picture
   end
@@ -16,6 +18,7 @@ class ListingsController < ApplicationController
   end
 
   def create
+    # this method will allow listings to be created as well as the listing's categories which is connected via a join table in a many-to-many relationship.
     @listing = current_user.listings.new(listing_params)
     @listing.listings_categories.build(category_id: params[:listing][:category_id])
 
@@ -44,10 +47,12 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
+    # strong params used only to allow the below parameters to avoid malicious code
     params.require(:listing).permit(:title, :description, :gluten_free, :price, :vegan, :dairy_free, :delivery, :picture, :name, categories_attributes: [:name])
   end
 
   def find_listing
+    # this method will be invoked for some other methods such as show, edit, update, destroy
     @listing = Listing.find(params[:id])
   end
 end
